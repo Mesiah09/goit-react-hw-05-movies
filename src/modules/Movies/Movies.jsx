1afdffd5dd342a59d5d10442bb5d36bde64cc178
Myroslav Vecheruk <mvecheruk@gmail.com>
2022-06-12 16:55:21 +0300

    1

--- src/modules/Movies/Movies.jsx
+++ src/modules/Movies/Movies.jsx
@@ -1,14 +1,19 @@
-import MoviesItem from './MoviesItem';
 import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
+
 import s from './movies.module.scss';
 export default function Movies({ data }) {
-  return (
-    <ul className={s.list}>
-      {data.map(e => (
-        <MoviesItem data={e} key={e.id} />
-      ))}
-    </ul>
-  );
+  const location = useLocation();
+  const elements = data.map(({ id, title }) => {
+    return (
+      <li key={id} className={s.item}>
+        <Link state={{ from: location }} to={`/movies/${id}`}>
+          {title}
+        </Link>
+      </li>
+    );
+  });
+  return <ul className={s.list}>{elements}</ul>;
 }
 Movies.defaultProps = {
   data: [],
